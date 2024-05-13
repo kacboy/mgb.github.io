0a636024d3d3a8a5546aef3f4f951e762095abec
kacboy <19754966+kacboy@users.noreply.github.com>
2024-05-13 12:03:27 -0700

    Update mc.js

--- js/mc.js
+++ js/mc.js
@@ -22,7 +22,7 @@ $.getJSON(url, function (r) {
   const versionNumber = r.version.name.split(" ", 2);
 
   // if (r.players.sample.length > 0) { pl = '<br>' + r.players.sample[0].name; }
-  $('#rest').html('<b>SERVER ONLINE!</b><br>Message me to be whitelisted.<br>Bedrock players use port 19132.<br><br><p>Discord: <a href="https://discord.gg/5mztRFs">https://discord.gg/5mztRFs</a></p><br><b>MOTD:</b> ' + r.description.replace(/§(.+?)/gi, '') + '<br><b>Version:</b> ' + versionNumber[1] + '<br><br><b>Players Online:</b> ' + r.players.online + '/' + r.players.max + '<br><ul>' + pl + '</ul>');
+  $('#rest').html('<b>SERVER ONLINE!</b><br>Message me to be whitelisted.<br>Bedrock players use port 19132.<br><br><p>Discord: <a href="https://discord.gg/5mztRFs">https://discord.gg/5mztRFs</a></p><br><b>MOTD:</b> ' + r.description.replace(/§(.+?)/gi, '') + '<br><b>Version:</b> ' + r.version.name + '<br><br><b>Players Online:</b> ' + r.players.online + '/' + r.players.max + '<br><ul>' + pl + '</ul>');
 
   $('#favicon').attr('src', r.favicon);
 
