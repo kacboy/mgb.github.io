d3f0bd530ca7ab72ff0f2cbddcad5d94e9431824
Marcus Blackstock <19754966+kacboy@users.noreply.github.com>
2025-07-11 10:15:22 -0700

    Update mc.js

--- js/mc.js
+++ js/mc.js
@@ -1,57 +1,48 @@
-//based on a pen by @robinselmer
-// https://codepen.io/nodws/pen/ZLLXXq
-var url = "https://api.minetools.eu/ping/mc.marcusblackstock.com";
-//https://mcapi.us/server/status?ip=666.fluctis.com&port=25757
+var url = "https://api.mcsrvstat.us/3/mc.marcusblackstock.com";
+
 $.getJSON(url, function (r) {
-  //data is the JSON string
-  if (r.error) {
+  if (!r.online) {
     $('#rest').html('<b>Server Offline</b><br><p>Discord: <a href="https://discord.gg/5mztRFs">https://discord.gg/5mztRFs</a></p>');
-    return false;
+    return;
   }
+
+  // Handle player list (may not exist or may be empty)
   var pl = '';
-
-
-  for (i = 0; i < r.players.sample.length; i++) {
-    var myobj = r.players.sample[i].name;
-    // for (var name in myobj) {
-    pl += '<li>' + myobj + '</li>';
-    // }
+  if (r.players && r.players.online > 0 && r.players.sample) {
+    for (var i = 0; i < r.players.sample.length; i++) {
+      pl += '<li>' + r.players.sample[i].name + '</li>';
+    }
+  } else {
+    pl = '<li>No players online</li>';
   }
 
-  // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_split      
-  const versionNumber = r.version.name.split(" ", 2);
+  // Handle MOTD
+  var motd = 'N/A';
+  if (r.motd && r.motd.clean && r.motd.clean.length > 0) {
+    motd = r.motd.clean.join('<br>');
+  }
 
-  // if (r.players.sample.length > 0) { pl = '<br>' + r.players.sample[0].name; }
-  $('#rest').html('<b>SERVER ONLINE!</b><br>Message me to be whitelisted.<br>Open to Java and Bedrock players.<br>Use port 19132 for Bedrock Edition.<br><br><p>Discord: <a href="https://discord.gg/5mztRFs">https://discord.gg/5mztRFs</a></p><br><b>MOTD:</b> ' + r.description.replace(/§(.+?)/gi, '') + '<br><b>Version:</b> ' + r.version.name + '<br><br><b>Players Online:</b> ' + r.players.online + '/' + r.players.max + '<br><ul>' + pl + '</ul>');
+  // Software + Version
+  var software = r.software ? r.software : '';
+  var version = r.version ? r.version : (r.protocol && r.protocol.name ? r.protocol.name : 'Unknown');
+  var softwareVersion = software ? software + ' ' + version : version;
 
-  $('#server-icon-img').attr('src', r.favicon);
+  // Handle port
+  var port = (r.port);
 
+  // Handle icon
+  if (r.icon) {
+    $('#server-icon-img').attr('src', r.icon);
+  }
+
+  // Display server info
+  $('#rest').html(
+    '<b>SERVER ONLINE!</b><br>Message me to be whitelisted.<br>' +
+    'Open to Java and Bedrock players.<br>Use port <b>' + port + '</b> for Bedrock Edition.<br><br>' +
+    '<p>Discord: <a href="https://discord.gg/5mztRFs">https://discord.gg/5mztRFs</a></p><br>' +
+    '<b>MOTD:</b> ' + motd + '<br>' +
+    '<b>Version:</b> ' + version + '<br><br>' +
+    '<b>Players Online:</b> ' + r.players.online + '/' + r.players.max + '<br>' +
+    '<ul>' + pl + '</ul>'
+  );
 });
-
-// https://stackoverflow.com/questions/56279807/is-it-possible-to-automatically-have-the-last-updated-date-on-my-website-changed/56280126
-// https://codepen.io/chelsslehc/pen/eaVXQj?editors=1111
-// const desiredRepo = "MCMap";
-// const dateTagClass = ".date";
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function () {
-//   if (this.readyState == 4 && this.status == 200) {
-//     let repos = JSON.parse(this.responseText);
-
-//     //https://stackoverflow.com/questions/4726040/javascript-adding-zeros-to-the-beginning-of-a-string-max-length-4-chars
-//     function zeroPad(num) {
-//       return num.toString().padStart(2, "0");
-//     }
-
-//     repos.forEach((repo) => {
-//       if (repo.name == desiredRepo) {
-//         var lastUpdated = new Date(repo.pushed_at);
-//         var day = zeroPad(lastUpdated.getUTCDate()) - 1;
-//         var month = zeroPad(parseInt(lastUpdated.getUTCMonth()) + 1);
-//         var year = lastUpdated.getUTCFullYear();
-//         $(dateTagClass).text(`Last updated: ${month}/${day}/${year}`);
-//       }
-//     });
-//   }
-// };
-// xhttp.open("GET", "https://api.github.com/users/kacboy/repos", true);
-// xhttp.send();
